0192fa4ea637391c2fef33f8f1693a437be833a6
JpSCabral <cabral.santosjp@hotmail.com>
2025-08-14 16:39:27 -0300

    Add baseUrl for login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,36 +1,26 @@
+import userData from "../fixtures/userData.json";
+
 describe("template spec", () => {
   const selectorList = {
     usernameField: "[name='username']",
     passwordField: "[type='password']",
     buttonField: "[type='submit']",
-    alertField: ".oxd-alert",
+    alertField: "[role='alert']",
     addressLink:
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+      "",
     addressLoginLink: "/web/index.php/dashboard/index",
-    gridDashboard: ".orangehrm-dashboard-grid"
+    gridDashboard: ".orangehrm-dashboard-grid",
   };
 
-const userData = {
-userSucess: {
-  username: 'Admin',
-  password: 'admin123'
-},
-userFail: {
-  username: '123',
-  password: '123'
-}
-
-}
-
   it("Login - success", () => {
-    cy.visit(selectorList.addressLink);
+    cy.visit('/auth/login');
     cy.get(selectorList.usernameField).type(userData.userSucess.username);
     cy.get(selectorList.passwordField).type(userData.userSucess.password);
     cy.get(selectorList.buttonField).click();
     cy.location("pathname").should("equal", selectorList.addressLoginLink);
     cy.get(selectorList.gridDashboard);
   });
-  it.skip("Login - failed", () => {
+  it("Login - failed", () => {
     cy.visit(selectorList.addressLink);
     cy.get(selectorList.usernameField).type(userData.userFail.username);
     cy.get(selectorList.passwordField).type(userData.userFail.password);
